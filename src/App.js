e3b44665564c3453e58e222e9acc8386bd767e18
Abdelrahman Osama <69199570+AbdelrahmanOsamaAhmed@users.noreply.github.com>
2023-03-20 09:43:08 +0200

    Article section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
-import MainSection from './components/MainSection/MainSection';
-
+import "./App.css";
+import Article from "./components/ArticleSection/Article";
+import MainSection from "./components/MainSection/MainSection";
+import article1 from "./assets/article1.png";
+import article2 from "./assets/article2.png";
 function App() {
   return (
-    <MainSection />
+    <>
+      <MainSection />
+      <Article title="Work out at home for free." img={article1} />
+      <Article
+        title="Get more with low-cost training programs and advanced features."
+        textRight={true}
+        img={article2}
+      />
+    </>
   );
 }
 
---
+++ src/components/ArticleSection/Article.js
@@ -0,0 +1,55 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import Section from "../UI/Section";
+import classes from "./Article.module.css";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import article1 from "../../assets/article1.png";
+const Article = ({ textRight, title, img }) => {
+  if (textRight) {
+    return (
+      <Section>
+        <div className={classes.wrapper}>
+          <img src={img} alt="article picture" />
+          <div className={classes.text}>
+            <h1>{title}</h1>
+            <p>
+              We believe fitness should be accessible to everyone, everywhere,
+              regardless of income or access to a gym. With hundreds of
+              professional workouts, healthy recipes and informative articles,
+              as well as one of the most positive communities on the web, you’ll
+              have everything you need to reach your personal fitness goals –
+              for free!
+            </p>
+            <button className={classes["see-more"]}>
+              See More{" "}
+              <FontAwesomeIcon style={{ color: "white" }} icon={faArrowRight} />
+            </button>
+          </div>
+        </div>
+      </Section>
+    );
+  }
+  return (
+    <Section>
+      <div className={classes.wrapper}>
+        <div className={classes.text}>
+          <h1>Work out at home for free.</h1>
+          <p>
+            We believe fitness should be accessible to everyone, everywhere,
+            regardless of income or access to a gym. With hundreds of
+            professional workouts, healthy recipes and informative articles, as
+            well as one of the most positive communities on the web, you’ll have
+            everything you need to reach your personal fitness goals – for free!
+          </p>
+          <button className={classes["see-more"]}>
+            See More{" "}
+            <FontAwesomeIcon style={{ color: "white" }} icon={faArrowRight} />
+          </button>
+        </div>
+        <img src={img} alt="article picture" />
+      </div>
+    </Section>
+  );
+};
+
+export default Article;
